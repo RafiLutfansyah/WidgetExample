c7ed398d07185ec18f89603fc6b03f19866df947
Rafi Lutfansyah <rafilutfansyah@gmail.com>
2017-10-01 10:49:06 +0700

    First Commit

---
+++ app/src/main/java/id/rafilutfansyah/widgetexample/MainActivity.java
@@ -0,0 +1,16 @@
+package id.rafilutfansyah.widgetexample;
+
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RemoteViews;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
